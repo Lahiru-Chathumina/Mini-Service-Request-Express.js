aa03ebf6972e1c73624e5a59968a999832fddfd4
lahiru-chathumina <chathuminabandaralahiru@gmail.com>
2026-05-15 10:12:46 +0530

    update my code

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const jobRoutes = require("./routes/job.routes");
 const errorMiddleware = require("./middleware/error.middleware");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/jobs", jobRoutes);
