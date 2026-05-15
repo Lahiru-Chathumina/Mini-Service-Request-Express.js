f7ed0560f79bdf4e30f129f992026d12526b5056
lahiru-chathumina <chathuminabandaralahiru@gmail.com>
2026-05-15 09:32:48 +0530

    add JobRequest model to define job request schema

---
+++ src/models/JobRequest.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const jobSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    category: String,
+    location: String,
+    contactName: String,
+    contactEmail: {
+      type: String,
+      match: /.+\@.+\..+/
+    },
+    status: {
+      type: String,
+      enum: ["Open", "In Progress", "Closed"],
+      default: "Open"
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("JobRequest", jobSchema);
